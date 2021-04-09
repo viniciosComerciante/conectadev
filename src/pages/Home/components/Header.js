b1259ecf6d415f8f61059157b5f7627ba9730d8c
vinicios <vinicompif@gmail.com>
2021-04-09 18:10:39 -0300

    initial commit

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header">
+        <div className="toolbar">
+          <div>
+            <span>Conecta Dev</span>
+          </div>
+          <div>
+            <button>Novo Post</button>
+            <span>IMG1</span>
+            <span>IMG2</span>
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header
